6775a3981cb8390d06ded90a97a3f34bcbd64278
David Phipps <elminestrone@gmail.com>
2020-08-26 16:18:57 -0500

    earthquake color/popups

--- Simple_Map/static/js/logicStep3.js
+++ Simple_Map/static/js/logicStep3.js
@@ -38,7 +38,7 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
         return {
             opacity: 1,
             fillOpacity: 1,
-            fillColor: "#ffae42",
+            fillColor: getColor(feature.properties.mag),
             color: "#000000",
             radius: getRadius(feature.properties.mag),
             stroke: true,
@@ -54,6 +54,25 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
         }
         return magnitude * 4;
     }
+    // This function determines the color of the circle based on the magnitude of the earthquake.
+    function getColor(magnitude) {
+        if (magnitude > 5) {
+            return "#ea2c2c";
+        }
+        if (magnitude > 4) {
+            return "#ea822c";
+        }
+        if (magnitude > 3) {
+            return "#ee9c00";
+        }
+        if (magnitude > 2) {
+            return "#eecc00";
+        }
+        if (magnitude > 1) {
+            return "#d4ee00";
+        }
+        return "#98ee00";
+    }
 
     // Creating a GeoJSON layer with the retrieved data.
     L.geoJson(data, {
@@ -63,7 +82,7 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
             return L.circleMarker(latlng);
         },
         onEachFeature: function (feature, layer) {
-            layer.bindPopup("<h3> Magnitude: " + feature.properties.mag + "</h3> <hr> <h3> Location: " + feature.properties.place + "</h3>");
+            layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
         },
         // We set the style for each circleMarker using our styleInfo function.
         style: styleInfo
